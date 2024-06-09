414ab881646e0aa7a735a772d9ec108e938dc4be
Marcelo <marcelo17082000@gmail.com>
2024-06-08 23:54:16 -0300

    Project created

---
+++ src/main/java/com/example/layeredarchexample/LayeredArchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.layeredarchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredArchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayeredArchExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/layeredarchexample/LayeredArchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.layeredarchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredArchExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
